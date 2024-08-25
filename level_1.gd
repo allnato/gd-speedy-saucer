58906d2a5ebce5d83e35fe57161ddaa27d3756d0
Allendale Nato <natoallendale@gmail.com>
2024-08-25 18:20:18 +0800

    Add maze

--- level_1.gd
+++ level_1.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-
+const START_POSITION: Vector2 = Vector2(281, 173)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_maze_body_exited(body: Node2D) -> void:
+	body.queue_free()
+	get_tree().call_deferred("reload_current_scene")
--- player.gd
+++ player.gd
@@ -1,11 +1,13 @@
 extends RigidBody2D
 
-const MOVE_FORCE: int = 1000
-
-var right: bool = true
+class_name Player
+const MOVE_FORCE: int = 300
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	gravity_scale = 0
+	#limit_camera()
+
+
 
 func _physics_process(_delta: float) -> void:
 
@@ -20,3 +22,10 @@ func _physics_process(_delta: float) -> void:
 
 	if Input.is_action_pressed("move_down"):
 		apply_force(Vector2.DOWN * MOVE_FORCE)
+
+
+func limit_camera() -> void:
+	$Camera2D.limit_left = 0
+	$Camera2D.limit_top = 0
+	$Camera2D.limit_right = 1152
+	$Camera2D.limit_bottom = 648
